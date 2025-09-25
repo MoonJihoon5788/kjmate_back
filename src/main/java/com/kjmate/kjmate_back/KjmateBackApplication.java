4849e97fd16cc1468ee9be861853c943abf14390
MoonJihoon5788 <munjihun3824@naver.com>
2025-09-25 21:58:40 +0900

    create project

---
+++ src/main/java/com/kjmate/kjmate_back/KjmateBackApplication.java
@@ -0,0 +1,13 @@
+package com.kjmate.kjmate_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KjmateBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KjmateBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kjmate/kjmate_back/KjmateBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjmate.kjmate_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KjmateBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
